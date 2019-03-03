70a8399e5d52aa88c624725a37dfb9e35809c69e
Pei Hsuan Hung <afcidk@gmail.com>
2019-03-03 17:25:38 +0800

    Add dependencies

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 CUR_DIR=$(pwd)
+
+### Install Dependencies ###
+sudo apt-get install screen
+
+if [ -z "$(command -v node)" ]; then 
+	curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash - 
+	sudo apt-get install -y nodejs
+fi
 
 npm install
 sed -i "s|/path/to/the/exe|$CUR_DIR/telegram|g" "telegram.desktop"
